3edd3a674978b29eb3c6b3e94674bceaa17f8390
Emmanouil Kampitakis <emmanouil.kampitakis@1und1.de>
2016-09-15 14:56:42 +0000

    Now the locksreen is saved to separate path in case the python script can not convert the image. Now the last successfull image is used

--- lockscreen.sh
+++ lockscreen.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 FILEPATH="/tmp/screen.png"
+FILEPATH_LOCK="/tmp/lockscreen.png"
 
 import -window root $FILEPATH
 
 #convert -spread 15 -implode 0.4 -negate $FILEPATH $FILEPATH
-~/.config/i3/scripts/fourier.py $FILEPATH $FILEPATH
+~/.config/i3/scripts/fourier.py $FILEPATH $FILEPATH_LOCK
 
 #mute all audio
 amixer -q sset Master,0 mute
 
-i3lock --image=$FILEPATH
+i3lock --image=$FILEPAH_LOCK
 #rm $FILEPATH
