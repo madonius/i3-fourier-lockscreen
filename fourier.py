d4f6ee2e70a5d2f48bfaf2a34b9f0992b321ea86
Emmanouil Kampitakis <info+git@kampitakis.de>
2016-10-07 20:50:59 +0000

    Added colour to the lockscreen, reduced the resolution since colour could revela more about the content

--- fourier.py
+++ fourier.py
@@ -1,25 +1,30 @@
 #!/usr/bin/env python3
 
-import numpy as np
+#import numpy as np
+from scipy import fftpack
+from scipy import absolute
+from scipy.misc import imsave, imread, imshow
 from PIL import Image
 import sys
 
-image = np.asarray(Image.open(sys.argv[1]).convert("P"))
-fft = np.fft.fft2(image)
-fshift = np.fft.fftshift(fft)
-mag_spectrum = 20*np.log(np.abs(fshift))
+image = imread(sys.argv[1])
+fft = fftpack.fftn(image)
+fshift = fftpack.fftshift(fft)
 
-rows, cols = image.shape
-red_const = 300
+rows = len(fft)
+cols = len(fft[0])
+red_const = int(3*cols/4)
 
-crow, ccol = rows/2, cols/2
+crow, ccol = int(rows/2), int(cols/2)
 fshift[:int(crow-rows/red_const), :int(ccol-cols/red_const)] = 0
 fshift[:int(crow-rows/red_const), int(ccol+cols/red_const):] = 0
 fshift[int(crow+rows/red_const):, int(ccol+cols/red_const):] = 0
 fshift[int(crow+rows/red_const):, :int(ccol-cols/red_const)] = 0
 
-f_ishift = np.fft.ifftshift(fshift)
-img_back = abs(np.fft.ifft2(f_ishift))
+f_ishift = fftpack.ifftshift(fshift)
+img_back = fftpack.ifftn(f_ishift)
+img_back = absolute(img_back)
 
-result = Image.fromarray((img_back).astype(np.uint8))
-result.save(sys.argv[2], "JPEG", quality=80, optimize=True, progressive=True)
+print(img_back[100])
+result = imsave(sys.argv[2], img_back)
+#result.save(sys.argv[2], "JPEG", quality=80, optimize=True, progressive=True)
