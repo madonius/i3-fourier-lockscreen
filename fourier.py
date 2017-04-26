77881f1e84691e8be5951fb61d8677bf43c37f34
Emmanouil Kampitakis <emmanouil.kampitakis@1und1.de>
2017-04-26 14:23:01 +0000

    Add measurement points to determine what takes the longest

--- fourier.py
+++ fourier.py
@@ -2,13 +2,20 @@
 
 from scipy import absolute, fftpack
 from scipy.misc import imsave, imread, imresize
+from datetime import datetime as dt
 from PIL import Image
 import sys
 
+start_time = dt.now()
+
 image = imread(sys.argv[1])
+print("Read: ", dt.now()-start_time)
 image = imresize(image, 0.5, interp='bilinear')
+print("Rescaled: ", dt.now()-start_time)
 fft = fftpack.fftn(image)
+print("Fourier-transformed: ", dt.now()-start_time)
 fshift = fftpack.fftshift(fft)
+print("Shifted: ", dt.now()-start_time)
 
 rows = len(fft)
 cols = len(fft[0])
@@ -21,11 +28,15 @@ fshift[:int(crow-rows/red_const), :int(ccol-cols/red_const)] = 0
 fshift[:int(crow-rows/red_const), int(ccol+cols/red_const):] = 0
 fshift[int(crow+rows/red_const):, int(ccol+cols/red_const):] = 0
 fshift[int(crow+rows/red_const):, :int(ccol-cols/red_const)] = 0
+print("Reduced information: ", dt.now()-start_time)
 
 f_ishift = fftpack.ifftshift(fshift)
 img_back = fftpack.ifftn(f_ishift)
 img_back = absolute(img_back)
+print("Retransformed: ", dt.now()-start_time)
 
 img_back = imresize(img_back, 2.0, interp='bilinear')
+print("Resized: ", dt.now()-start_time)
 
 result = imsave(sys.argv[2], img_back)
+print("Saved: ", dt.now()-start_time)
