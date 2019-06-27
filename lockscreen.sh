33e4b3505fb56a5489f6170ffedb420ae52a316b
Emmanouil Kampitakis <info+git@kampitakis.de>
2019-06-27 18:33:56 +0200

    Make sway adaptations

--- lockscreen.sh
+++ lockscreen.sh
@@ -3,13 +3,18 @@
 FILEPATH="/tmp/screen.png"
 FILEPATH_LOCK="/tmp/lockscreen.png"
 
-import -window root $FILEPATH
+grim -t png $FILEPATH
 
 #convert -spread 15 -implode 0.4 -negate $FILEPATH $FILEPATH
-$XDG_CONFIG_HOME/i3/scripts/i3-fourier-lockscreen/fourier.py $FILEPATH $FILEPATH_LOCK
+
+if [ -z $XDG_CONFIG_HOME ]; then
+	XDG_CONFIG_HOME=$HOME/.config
+fi
+
+$XDG_CONFIG_HOME/i3/scripts/fourier.py $FILEPATH $FILEPATH_LOCK
 
 #mute all audio
 amixer -q sset Master,0 mute
 
-i3lock --image=$FILEPATH_LOCK
-#rm $FILEPATH
+swaylock --tiling --image $FILEPATH_LOCK
+rm $FILEPATH
